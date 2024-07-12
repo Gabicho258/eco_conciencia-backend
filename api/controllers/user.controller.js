4cf8d7662699d2193f952dd033b4849b78f89f0a
Gabriel Antony <gmamanican@unsa.edu.pe>
2024-07-11 20:17:07 -0500

    add login authentication

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-
+import jwt from "jsonwebtoken";
 import { User } from "../models/index.js";
 
 export const getAllUsers = async (req, res) => {
@@ -29,6 +29,7 @@ export const createUser = async (req, res) => {
     const existingUser = await User.findOne({ email });
     if (existingUser)
       return res.status(400).json({ message: "Email already exists" });
+    console.log(email, password);
     const passwordHash = await bcrypt.hash(password, 10);
     const userToCreate = { ...req.body, password: passwordHash };
     const user = new User(userToCreate);
@@ -51,4 +52,32 @@ export const updateUser = async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
+};
+
+export const login = async (req, res) => {
+  // Verificamos la petición
+  const { email, password } = req.body;
+  if (!(email && password))
+    return res.status(400).json({ message: "Bad request" });
+
+  // Comprobamos que exista un usuario con el correo ingresado
+  const user = await User.findOne({ email });
+  if (!user) return res.status(404).json({ message: "User not found" });
+
+  // Validamos la contraseña encriptada
+  bcrypt.compare(password, user.password, (error, isValid) => {
+    if (isValid) {
+      jwt.sign(
+        { email: user.email },
+        process.env.SECRET_KEY,
+        (error, token) => {
+          if (!error) {
+            res.status(200).json({ token, user });
+          } else res.status(401).json({ message: "Error generating token" });
+        }
+      );
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  });
 };
